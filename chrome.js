24fef0f6fd83b6e4ab56502dfe881946d79052ca
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-08 13:50:49 +0000

    username localstorage save

---
+++ chrome.js
@@ -0,0 +1,22 @@
+const loginForm = document.getElementById('login-form');
+const loginInput = loginForm.querySelector('input');
+//const loginButton = loginForm.querySelector('button');
+const greeting = document.getElementById('greeting');
+const HIDDEN_CLASSNAME = 'hidden';
+function onLoginBtnClick(){
+  const username = loginInput.value;
+  console.log(username);
+}
+
+//loginButton.addEventListener('click', onLoginBtnClick);
+loginForm.addEventListener(
+  'submit',onLoginSubmit);
+function onLoginSubmit(event){
+  event.preventDefault();//이벤트가 발생했을때 기본동작을 막는다.
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username",username);
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
