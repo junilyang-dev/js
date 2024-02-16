20179940ed5971c98a440080a4273b32bdcbcc62
junilyang-dev <159020750+junilyang-dev@users.noreply.github.com>
2024-02-16 11:59:50 +0000

    background img Api use

--- js/background.js
+++ js/background.js
@@ -1,3 +1,4 @@
+/*
 const images = [
   "0.jpg","1.jpg","2.jpg"
 ];
@@ -7,3 +8,12 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.querySelector(".background-img");
 
 bgImage.src = `/image/${chosenImage}`;
+*/
+
+const bgImage = document.querySelector(".background-img");
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+// 랜덤 이미지를 가져와 배경으로 설정
+  bgImage.src = `https://picsum.photos/${width}/${height}`;
